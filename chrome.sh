b0aaf9161f11775143b1db45c3e84f644aa8c7af
Your Name <you@example.com>
2026-04-04 18:43:09 +0300

    chrome headless now

--- chrome.sh
+++ chrome.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Run this once in a separate terminal, then start the bot with: npx ts-node index.ts
+#this way you dont have to reopen whatsapp or rerun server each time
 "/Applications/Google Chrome updated yay.app/Contents/MacOS/Google Chrome" \
+  --headless=new \
   --remote-debugging-port=9222 \
   --no-sandbox \
   --user-data-dir=/tmp/wa-chrome
