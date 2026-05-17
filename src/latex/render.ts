7aa0ba2ffbe410937e3f9d5e475df3260594ded8
Your Name <you@example.com>
2026-05-17 20:21:14 +0300

    zoom out stickers a bit

--- src/latex/render.ts
+++ src/latex/render.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import sharp from 'sharp';
 
 export async function renderLatex(latex: string): Promise<Buffer> {
-    const url = `https://latex.codecogs.com/png.image?\\dpi{300}${encodeURIComponent(latex)}`;
+    const url = `https://latex.codecogs.com/png.image?\\dpi{600}\\bg{white}${encodeURIComponent(latex)}`;
     const res = await axios.get(url, { responseType: 'arraybuffer', timeout: 10000 });
     return sharp(Buffer.from(res.data))
-        .flatten({ background: { r: 255, g: 255, b: 255 } })
-        .resize(512, 512, { fit: 'contain', background: { r: 255, g: 255, b: 255 } })
+        .resize(440, 440, { fit: 'contain', background: { r: 255, g: 255, b: 255 } })
+        .extend({ top: 36, bottom: 36, left: 36, right: 36, background: { r: 255, g: 255, b: 255 } })
         .webp()
         .toBuffer();
 }
